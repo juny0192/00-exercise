2a132aa521a6dc3d69c304fa7f5dd233185979f9
Byungjun Jung <jun@Byungjuns-MacBook-Pro.local>
2021-03-29 16:56:23 -0400

    exercise file upload

---
+++ 00-challenge.js
@@ -0,0 +1,68 @@
+class Vehicle {
+  constructor(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+
+  honk() {
+    return 'Beep!';
+  }
+
+  toString() {
+    return `The vehicle is a ${this.make} ${this.model} from ${this.year}.`;
+  }
+}
+
+let myFirstVehicle = new Vehicle('Honda', 'Monster Truck', 1999);
+
+class Car extends Vehicle {
+  constructor(make, model, year) {
+    super(make, model, year);
+    this.numWheels = 4;
+  }
+}
+
+let myFirstCar = new Car('Toyota', 'Corolla', 2005);
+
+class Motorcycle extends Vehicle {
+  constructor(make, model, year) {
+    super(make, model, year);
+    this.numWheels = 2;
+  }
+
+  revEngine() {
+    return 'VROOM!';
+  }
+}
+
+let myFirstMotorcycle = new Motorcycle('Honda', 'Nighthawk', 2000);
+
+class Garage {
+  constructor(capacity) {
+    this.capacity = capacity;
+    this.vehicles = [];
+  }
+
+  add(vehicle) {
+    if (this.vehicles.length >= this.capacity) {
+      return "Sorry, we're full.";
+    }
+
+    if (
+      vehicle.constructor.name === 'Car' ||
+      vehicle.constructor.name === 'Motorcycle'
+    ) {
+      if (vehicle.constructor.name === 'Car') {
+        console.log('new Car added');
+        return this.vehicles.push('Car');
+      }
+      if (vehicle.constructor.name === 'Motorcycle') {
+        console.log('new Motorcycle added');
+        return this.vehicles.push('Motorcycle');
+      }
+    } else {
+      return 'Only vehicles are allowed in here!';
+    }
+  }
+}
